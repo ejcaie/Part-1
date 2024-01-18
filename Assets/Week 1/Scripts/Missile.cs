bd7c2c24bdccecba084f87c00e6c4d4454d9756f
Evan Caie <caiee@shernet.sheridancollege.ca>
2024-01-18 14:20:17 -0500

    Task 1 complete. Missile hits player.

--- Assets/Week 1/Scripts/Missile.cs
+++ Assets/Week 1/Scripts/Missile.cs
@@ -4,16 +4,29 @@ using UnityEngine;
 
 public class Missile : MonoBehaviour
 {
-    float speed = 7f;
-    // Start is called before the first frame update
-    void Start()
-    {
+    public float speed = 7f;
+    Rigidbody2D rigidbody;
+    Vector2 direction = new Vector2 (10, 0);
 
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(speed * Time.deltaTime, 0, 0);
+      //  transform.Translate(speed * Time.deltaTime, 0, 0);
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody.MovePosition(rigidbody.position + direction * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Missile hit something");
+        Destroy(gameObject);
     }
 }
